f46e0387d06d2e06a6323b2804583f640da1b2ec
Sxfrui <86210715+Sxfrui@users.noreply.github.com>
2022-03-13 21:28:28 -0400

    New Code

---
+++ New code .py
@@ -0,0 +1,5 @@
+print("Hello, welcome to our shop.")
+name = input("What is your name? ")
+input("hello, " + name + ". Would you like to check out our menu? ")
+menu = "Latte, Cappacino, Barrista, Ice Coffee, and Regular coffee"
+print(name + ". This is our menu, please choose an item of your liking " + menu)
