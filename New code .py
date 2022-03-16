347a37ca295527a6fc7b05e68f8e6f449c06a352
Sxfrui <86210715+Sxfrui@users.noreply.github.com>
2022-03-15 23:31:38 -0400

    Update New code .py

--- New code .py
+++ New code .py
@@ -1,5 +1,28 @@
-print("Hello, welcome to our shop.")
 name = input("What is your name? ")
+
+if name == "ben" or name == "thomas":
+    evil_status = input("Are you evil?\n")
+    if evil_status == "Yes":
+     print("You're not allowed here!")
+    else: print("Welcome!")
+    exit()
+
+
 input("hello, " + name + ". Would you like to check out our menu? ")
-menu = "Latte, Cappacino, Barrista, Ice Coffee, and Regular coffee"
-print(name + ". This is our menu, please choose an item of your liking " + menu)
+print("Hello, welcome to our shop.") 
+
+menu = "Latte, Cappacino, Barrista, Ice Coffee, and Regular coffee."
+order = print(name + ". This is our menu, please choose an item of your liking " + menu)
+
+menu = input("What would you like? ")
+
+price = "8"
+quantity = input("How many would you like? ")
+total = int(price) * int(quantity)
+print("your total is: $" + str(total))
+
+if order == "Latte":
+    price = "13"
+else: 
+    price = "8"
+
